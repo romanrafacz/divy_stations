740247f808ff5dfe764cbd549821cb01d7bcae10
shoemonkey <romanr32@gmail.com>
2016-01-05 08:35:52 -0600

    error page added

--- src/main/java/com/divy/controller/CourseController.java
+++ src/main/java/com/divy/controller/CourseController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ProfileController {
+public class CourseController {
 	
-	@RequestMapping("/profile")
-	public String profile(){
-		return "profile";
+	@RequestMapping("/course")
+	public String course(){
+		return "courses";
 	}
 
 }
