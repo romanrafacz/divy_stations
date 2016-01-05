b55251e6ebdb123bcbe4d49ff232a528557b74c9
shoemonkey <romanr32@gmail.com>
2016-01-05 14:04:21 -0600

    UserController mapped

---
+++ src/main/java/com/divy/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.divy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.divy.service.UserService;
+
+@Controller
+public class UserController {
+	
+	private final UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService){
+		this.userService = userService;
+	}
+
+	@RequestMapping("/users")
+	public ModelAndView getUserPage(){
+		return new ModelAndView("users", "users", userService.getAllUsers() );
+	}
+}
