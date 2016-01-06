39e125b8b78b1820011affbb3eb52a0aaea7b1d7
shoemonkey <romanr32@gmail.com>
2016-01-06 10:02:09 -0600

    create form controller added

--- src/main/java/com/divy/controller/UserController.java
+++ src/main/java/com/divy/controller/UserController.java
@@ -1,9 +1,23 @@
 package com.divy.controller;
 
+import java.util.NoSuchElementException;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.divy.domain.UserCreateForm;
 import com.divy.domain.validator.UserCreateFormValidator;
 import com.divy.service.UserService;
 
@@ -23,5 +37,29 @@ public class UserController {
 		binder.addValidators(userCreateFormValidator);
 	}
 	
+	@RequestMapping("/user/{id}")
+	public ModelAndView getUserPage(@PathVariable Long id){
+		return new ModelAndView("user", "user", userService.getUserById(id)
+				.orElseThrow(() -> new NoSuchElementException(String.format("User=%s not found", id))));
+	}
+	
+	@RequestMapping(value = "/user/create", method = RequestMethod.GET)
+	public ModelAndView getUserCreatePage(){
+		return new ModelAndView("user_create", "form", new UserCreateForm());
+	}
+	
+	@RequestMapping(value = "/user/create", method=RequestMethod.POST)
+	public String handleUserCreateForm(@Valid @ModelAttribute("form") UserCreateForm form, BindingResult bindingResult){
+		if (bindingResult.hasErrors()) {
+			return "user_create";
+		}
+		try {
+			userService.create(form);
+		}catch (DataIntegrityViolationException e){
+			bindingResult.reject("email.exists", "Email already exists");
+			return "user_create";
+		}
+		return "redirect:/users";
+	}
 	
 }
